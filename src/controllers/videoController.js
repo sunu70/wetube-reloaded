400a11d7b480e17026941c5787b4affc8fa3191a
Sunu <yode4580@gmail.com>
2022-01-10 17:00:34 +0900

    Third commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,35 @@
-export const trending = (req, res) => res.send("home");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+// 변수는 마음대로 넣어도됨 ex) {pageTitle : "Home", content : "Home!"}
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("Edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
