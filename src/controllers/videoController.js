cd4449872d8edf86a8eca917197063fee68a934b
sunu <tee4500@naver.com>
2022-01-12 21:18:23 +0900

    first commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -46,3 +46,22 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
   // res.redirect()는, 브라우저가 redirect(자동으로 이동)하도록 하는거임
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { watch, getEdit, postEdit } from "../controllers/videoController";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 // = videoRouter.get("/:id(\\d+)/edit", getEdit)+videoRouter.post("/:id(\\d+)/edit", postEdit)
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
