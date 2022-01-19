787b92f8be43e1c1f90a07d3dd7cf72219c78f9c
Sunu <yode4580@gmail.com>
2022-01-19 17:40:18 +0900

    first commit

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -8,5 +9,11 @@ const userSchema = new mongoose.Schema({
   location: String,
 });
 
+userSchema.pre("save", async function () {
+  console.log("Users password", this.password);
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log("Hashed password", this.password);
+  // console.log("명칭", function)as
+});
 const User = mongoose.model("User", userSchema);
 export default User;
