9f06a6febb5e49f63b6faae3737d0e81cefb4673
sunu <tee4500@naver.com>
2022-01-30 13:59:36 +0900

    first commit

--- src/models/User.js
+++ src/models/User.js
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
 
 userSchema.pre("save", async function () {
-  console.log("Users password", this.password);
   this.password = await bcrypt.hash(this.password, 5);
-  console.log("Hashed password", this.password);
-  // console.log("명칭", function)as
 });
+
 const User = mongoose.model("User", userSchema);
 export default User;
