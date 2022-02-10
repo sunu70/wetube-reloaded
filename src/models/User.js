e819a324bffc9add4f2022fa2c9b24c2c918104d
Sunu <yode4580@gmail.com>
2022-02-10 17:30:48 +0900

    02.10-2

--- src/models/User.js
+++ src/models/User.js
@@ -16,7 +16,6 @@ userSchema.pre("save", async function () {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 5);
   }
-  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);
