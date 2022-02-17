39f069a84b22f9a4cce2015265f3b39807eb3ea8
sunu <tee4500@naver.com>
2022-02-17 23:11:59 +0900

    02.17

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -9,16 +9,26 @@ let videoFile;
 
 const handleDownload = async () => {
   const ffmpeg = createFFmpeg({ log: true });
+  // {log:true} 무슨일이 벌어졌는지 로그로 확인
   await ffmpeg.load();
+  // ffmpeg.load()에 await 사용하는 이유는 사용자가 소프트웨어를 사용하기 위해
 
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
   //"writeFile" ffmpeg의 가상의 세계에 파일 생성
 
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
   //-i = input,
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+  // URL.createObjectURL() 브라우저에서 파일을 가르키는 마법의 url
+
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
 };
