32c0210ce991c276053dde3144c9967f224ea437
Sunu <yode4580@gmail.com>
2022-02-17 17:20:24 +0900

    02.17 복습

---
+++ src/client/js/recorder.js
@@ -0,0 +1,60 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+let recorder;
+let videoFile;
+// let을 사용하면 외부 stream 정의 한 것을 부를수있음!!
+
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  //"writeFile" ffmpeg의 가상의 세계에 파일 생성
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  //-i = input,
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    // 이벤트가 종료가 되면 나올 때 씀
+    videoFile = URL.createObjectURL(event.data);
+    // createObjectURL 브라우저 메모리에서만 가능한 URL 생성
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+init();
+
+startBtn.addEventListener("click", handleStart);
